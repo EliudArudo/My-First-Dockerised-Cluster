e66c000c541e51913ff4f69c8f551e69ea03dfcd
EliudArudo <eliudarudo@gmail.com>
2019-02-22 19:25:37 +0300

    Removed cors from server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 const sendNumber = require('./controllers/sendNumber');
